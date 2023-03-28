59b7561ab252007b5d1cc0275c9f1f062a3b540f
Maxence M <maxence@empreintedigitale.fr>
2023-03-28 13:48:44 +0200

    feat(Colors): text and background color is now randomly selected from a predefined theme list

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+let colors = [
+    {color: '#230329', background: '#1a98ad'}, // Original theme
+    {color: 'blue', background: '#6ddccf'},
+    {color: 'dodgerblue', background: '#3f1328'},
+    {color: 'skyblue', background: '#144729'},
+    {color: 'indigo', background: '#bee261'},
+    {color: 'blueviolet', background: '#e1ee3a'},
+    {color: 'fuchsia', background: '#1e1849'},
+    {color: 'violet', background: '#3a1c51'},
+    {color: 'hotpink', background: '#5c0d48'}, // Pink and Plum
+    {color: 'red', background: '#080057'}, // Scarlet Red and Royal Purple
+    {color: 'orangered', background: '#1f1218'}, // Orangered and Black
+    {color: 'orange', background: '#782a22'}, // Orange and Chestnut
+    {color: 'gold', background: '#414972'}, // Gold and Midnight blue
+    {color: 'yellow', background: '#ad3c61'}, // Laser Lemon and Jazzberry Jam
+    {color: 'springgreen', background: '#10564b'} // Spring Green and Pine Green
+];
+
 let days = document.querySelector('#days');
 let hours = document.querySelector('#hours');
 let minutes = document.querySelector('#minutes');
@@ -16,3 +34,8 @@ function refreshCounter() {
 }
 
 setInterval(refreshCounter, 1000);
+
+let root = document.documentElement;
+let randomValue = Math.floor(Math.random() * (colors.length - 1 + 1)) + 1;
+root.style.setProperty('--background-color', colors[randomValue].background);
+root.style.setProperty('--color', colors[randomValue].color);
