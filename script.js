491138d556c0c6ca345c0ede1c981e72b35395fe
Maxence M <maxence@empreintedigitale.fr>
2023-05-12 14:45:03 +0200

    feat(End): adding the message to be displayed after the counter passes under zero

--- script.js
+++ script.js
@@ -27,13 +27,18 @@ let target = new Date('2023-05-12T18:00:00');
 function refreshCounter() {
     time = new Date(target.getTime() - new Date().getTime());
 
+    if(time <= 0){
+        document.querySelector('#main').innerHTML = '<span class="gone">I\'M GONE!<br>🎉🚀🎆<span>';
+        return clearInterval(interval);
+    }
+
     days.innerHTML = (Math.round(time / 8.64e7)).toString().padStart(2, '0');
     hours.innerHTML = (time.getHours()).toString().padStart(2, '0');
     minutes.innerHTML = (time.getMinutes()).toString().padStart(2, '0');
     seconds.innerHTML = (time.getSeconds()).toString().padStart(2, '0');
 }
 
-setInterval(refreshCounter, 1000);
+let interval = setInterval(refreshCounter, 1000);
 
 let root = document.documentElement;
 let randomValue = Math.floor(Math.random() * (colors.length - 1 + 1)) + 1;
